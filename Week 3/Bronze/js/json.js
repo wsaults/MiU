0db6bd6cda75c9552e3221a30ada95306d708309
William Saults <wsaults@email.itt-tech.edu>
2012-06-13 20:54:16 -0500

    Added the JQ mobile search feature.

---
+++ Week 3/Bronze/js/json.js
@@ -0,0 +1,33 @@
+// Project: Deliverable 1
+// Name: William Saults
+// Term: 0612
+
+var json = {
+	"item1": {
+		"itemSelect": ["Item:","Watch"],
+		"itemNumber": ["SKU:","ABC123"],
+		"itemColor": ["Color:","Black"],
+		"itemPrice": ["Price:","129.99"],
+		"quantity": ["Quantity:","1"],
+		"giftWrapped": ["Gift Wrapped:","Yes"],
+		"applyCouponCode": ["Apply Coupon Code:","No"],
+		"receiveEmail": ["Receive Email:","No"],
+//Radio "verify"["Verify:","1"],
+		"select": ["Item:","Watch"],
+		"textarea": ["Feedback:","I love Fossil!"]
+	},
+	
+	"item2": {
+		"itemSelect": ["Item:","Wallet"],
+		"itemNumber": ["SKU:","123ABC"],
+		"itemColor": ["Color:","Brown"],
+		"itemPrice": ["Price:","29.99"],
+		"quantity": ["Quantity:","1"],
+		"giftWrapped": ["Gift Wrapped:","No"],
+		"applyCouponCode": ["Apply Coupon Code:","No"],
+		"receiveEmail": ["Receive Email:","No"],
+//Radio "verify"["Verify:","1"],
+		"select": ["Item:","Wallet"],
+		"textarea": ["Feedback:","I also love Fossil!"]
+	}
+}
